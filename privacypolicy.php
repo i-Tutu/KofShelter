661afa35e8a490a2fbc76a4e1d9fece62352e044
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 02:56:21 +0000

    Policy Privacy file

---
+++ privacypolicy.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - Privacy and Policy</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	 	 <?php
+			include("head.php");
+		 ?>
+		
+		<div class="card bg-light mt-5 mr-5 ml-5">
+	   		<div class="card-header">Privacy Policy</div>
+	   		<div class="card-body">	
+				<div>
+					<p>
+						In order for the website to provide a safe and useful service, it is important for KofShelter to collect, use, and share personal information.
+					</p>
+
+					<p>
+						<b> Collection</b> 
+					</p>
+
+					<p>
+						Information posted on KofShelter is publicly available. If you choose to provide us with personal information, you are consenting to the transfer and storage of that information on our servers. We collect and store the following personal information:
+						<br>
+					-	Email address, contact information, and (depending on the service used) 		sometimes financial information
+						<br>
+					-	Other information, including users' IP address and standard web log 			information.
+					</p>
+
+					<p>
+						<b>Use</b>
+					</p>
+
+					<p>
+						We use users' personal information to:
+					</p>
+
+					<p>
+					-	Provide our services
+						<br>
+					-	Resolve disputes, collect fees, and troubleshoot problems
+						<br>
+					-	Encourage safe trading and enforce our policies 
+						<br>
+					-	Customize users experience, measure interest in our services
+						<br>
+					-	Improve our services and inform users about services and updates
+						<br>
+					-	Communicate marketing and promotional offers to you according to your 			preferences		
+					</p>
+
+					<p>
+						<b>Disclosure</b>
+					</p>
+
+					<p>
+						We don't sell or rent users' personal information to third parties for their marketing purposes without users' explicit consent. We may disclose personal information to respond to legal requirements, enforce our policies, respond to claims that a posting or other content violates other's rights, or protect anyone's rights, property, or safety.
+					</p>
+
+						
+		   		 	
+
+				</div>
+
+			</div>
+		</div>
+
+		<!-- Footer -->
+ 		<?php
+    	
+		include("footer.php");
+	
+		// include("../includethings/footertag.php");
+		?>
+		<!-- Footer -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
