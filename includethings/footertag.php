5e4427cf2082fecc0d0dfa2cdc224129e2e92016
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 03:09:35 +0000

    Includes file

---
+++ includethings/footertag.php
@@ -0,0 +1,6 @@
+
+  <!-- This is the footer for softtap - KofShelter -->
+
+   <!--  <div class="footer-other mt-5 mb-2 text-center">
+		<small>KofShelter - Powered by Softtap with <i class="fa fa-heart"></i></small>   
+	</div> -->
