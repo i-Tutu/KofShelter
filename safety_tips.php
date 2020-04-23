ff9faa582b65c850b67aebe4d4be2c1b36800c60
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 02:53:47 +0000

    Safety Tips file

---
+++ safety_tips.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - Safety Tips</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	     <?php
+			include("head.php");
+		 ?>
+
+
+	<div class="card bg-light mt-5 mr-5 ml-5">
+	   <div class="card-header">Safety Tips</div>
+	    <div class="card-body">
+	    	<div>
+	    		<p><b> Stay safe on KofShelter </b>
+	    		<br>
+               At KofShelter we are 100% committed to making sure that your experience on our site is as safe as possible.</p>
+	
+               <p>Here you can find advice on how to stay safe while trading on KofShelter.</p>
+				
+	    	    <p> <b>Use common sense. </b><br>
+	    		Avoid anything that appears too good to be true, such as  unrealistically low prices and promises of quick money.</p>
+	    			
+                 <b>Never give out financial information.</b><br>
+                This includes bank account details, Mobile Money/Bank info, and any other information that could be misused.
+				</p>
+	    		</p>
+			</div>
+		</div>
+	</div>
+
+		<!-- Footer -->
+		<?php
+		include("footer.php");
+	
+ 		// include("../includethings/footertag.php") 
+ 		?>
+	<!-- Footer -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
