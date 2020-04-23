c3e1bc2e72b393dd8e55919be67fbb8a7b880673
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 02:51:46 +0000

    About file

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - About</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	     <?php
+			include("head.php");
+		 ?>
+
+
+	<div class="card bg-light mt-5 mr-5 ml-5 md-5">
+	   <div class="card-header">About Us</div>
+	    <div class="card-body">
+	    	<p>
+	    		KofShelter.com is a place where landlords and landladies display their available rooms to rent to people who need shelter or home to stay. KofShelter brings homes in Koforidua to people who need home to rent. 
+
+	    	</p>
+
+			<p>
+				  In order to improve the renting of houses system in Koforidua as a means of digitalizing Kofcity, providing a virtual system online will go a long way to affect the development of Koforidua city positively. To meet the diverse needs of the Koforidua community, both rental housing and homeownership are important. Rental homes fulfill the needs of many families.
+			</p>
+			<p>	  
+				   For some, especially low and moderate-income families in high- cost markets, rental homes are the most financially realistic option. For some families, affordable rental housing is an important stepping stone that allows them to accumulate savings and prepare for homeownership. Renting allows you to live in a premium area that you might not be able to afford to buy.  It’s always easy to shift out of rental accommodation. Most people prefer renting a house when starting to live on their own.
+			 </p>
+			 <p>
+				  Renting of the house has become an important factor in modern-day society hence the need to have online rental housing where landlords and landladies can post or showcase their available house for rent to those who need rent is very important. 
+
+			</p>
+
+		</div>
+	</div>
+
+		<!-- Footer -->
+ 		<?php
+    	
+		include("footer.php");
+	
+		// include("../includethings/footertag.php");
+	?>
+	<!-- Footer -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
