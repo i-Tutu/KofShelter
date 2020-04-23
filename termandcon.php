237e54b19b63d096f7f09960b49ca6442b1ce24f
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 02:52:41 +0000

    Terms and Conditions file

---
+++ termandcon.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - Terms & Conditions</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	   <?php
+			include("head.php");
+		?>
+
+		<div class="card bg-light mt-5 mr-5 ml-5">
+	      <div class="card-header">Terms & Conditions </div>
+		    <div class="card-body">	
+		   		 <div>
+		   		 	
+					<p>
+						KofShelter is a service provided by Students of Koforidua Technical University (subject to your compliance with the Terms and Conditions set forth below). Please read these Terms and Conditions before using this Web Site.
+					</p>
+				<p>	<b>	General</b> </p>
+
+					<p>	Advertisers and users are responsible for ensuring that advertising content, text, images, graphics, video ("Content") uploaded for inclusion on KofShelter complies with all applicable laws. KofShelter assumes no responsibility for any illegality or any inaccuracy of the Content.
+					</p>
+					<p>
+					The advertisers and user guarantees that his or her Content do not violate any copyright, intellectual property rights or other rights of any person or entity, and agrees to release KofShelter from all obligations, liabilities and claims arising in connection with the use of (or the inability to use) the service.
+					</p>
+
+					<p>
+					 Advertisers agree that their Content can may be presented through KofShelter's partner sites under the same terms and conditions as on KofShelter.
+					 </p>
+					
+					<p> <b>Copyright</b> </p>
+
+					<p>Advertisers grant KofShelter perpetual, royalty-free, irrevocable, non-exclusive right and license to use, reproduce, modify, adapt, publish, translate, create derivative works from and distribute such Content or incorporate such Content into any form, medium, or technology now known or later developed.
+					</p> 
+
+					<p>The material (including the Content, and any other content, software or services) contained on KofShelter are the property of KofShelter, affiliates and/or third party licensors. Any intellectual property rights, such as copyright, trademarks, service marks, trade names and other distinguishing brands on the Website are the property of KofShelter. 
+		   		 	</p>
+
+				 </div>
+		    </div>
+		</div>
+
+	<?php
+
+		include("footer.php");
+	
+		// include("../includethings/footertag.php");
+	?>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
