28a56de29ff301ae0c1f7d9349b35e4def0c18bb
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 03:04:59 +0000

    FAQ file

---
+++ faq.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - FAQ</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	     <?php
+			include("head.php");
+		 ?>
+
+	<div class="card bg-light mt-5 mr-5 ml-5">
+	   <div class="card-header">Renting FAQs</div>
+	    <div class="card-body">
+	    	<p>
+	    	All your Frequently Asked Questions about renting a house or flat answered, including information about rent guarantors, referencing and deposits.
+			</p>
+	    	<p>
+	    		<b>	I need to find a place to rent. What do I do first?</b>
+				<br>
+			Before you start searching for your new home it’s a good idea to write down a budget. What are your current outgoings and what money do you have left each month to spend on rent? Take into account that, when you first move in, you will need to front a security deposit as well as the first month’s rent and a refundable holding deposit.
+            
+            </p>
+			<p>
+				<b>I've found a place I want to rent. Now what?</b>
+				<br>
+			If you haven’t already, make sure you go and view the property. If it’s a house-share, meet all the people you’ll be moving in with. The letting agent will ask you to sign a Tenancy Fee Declaration form which lists the services they will provide and the Permitted Payments expected from you, in line with the Tenant Fees Act 2019. The agent will then begin the referencing process to ensure you’re in a position to rent the property.	  
+			</p>
+			
+			<p>
+				<b>Why do I need to be referenced?</b>
+				<br>
+			The landlord needs to be sure that that their tenant won’t have any problems paying the rent on a monthly basis and that the tenant will take good care of their property. 
+			 </p>
+			 
+			 <p>
+			 	<b>What does referencing involve?</b>
+				<br>
+			Referencing is nothing to worry about. Tenants applying to rent need to give details of their employer and income, their previous address, and some bank account details. These will be checked to ensure they are able to commit to monthly rental payments.
+            
+			</p>
+			 <p>
+			 	<b>Do I need to show ID?</b>
+				<br>
+			As part of the referencing process we need to be sure a tenant is who they say they are. We will require a proof of residency (such as a utility or council tax bill from the last 3 months) and proof of ID (such as a passport or driving licence). 
+			</p>
+			 <p>
+			 	<b>What if there are problems with my reference?</b>
+				<br>
+			In some circumstances, a tenant may not be approved immediately via referencing. Obvious examples are students without a regular income, or someone leaving their family home for the first time with no renting history. This is not uncommon, and there are still options for tenants in this position. They could pay the rent for the full term up front, or seek out a guarantor.
+			</p>
+			 <p>
+			 	<b>What is a guarantor?</b>
+				<br>
+			If a tenant is not fully approved by the referencing process, they can ask a guarantor to support them. A guarantor (usually a parent or guardian) will agree to take joint responsibility for the rent for the property if the tenant fails to. Guarantors are required to pay any rent arrears (if the tenant does not pay) and for any damages costing more than the deposit.
+			</p>
+			 <p>
+			 	<b>What does a guarantor need to do?</b> 
+				<br>
+			A guarantor needs to go through the same referencing process as a tenant. The normal requirement is that they are employed and a UK resident, with sufficient earnings to cover the tenant’s rental commitment.
+			</p>
+			 <p>
+				<b>Why does my guarantor have to guarantee all tenants?</b> 
+				<br>
+			In the case of a house-share, the tenancy agreement makes all tenants jointly responsible for all rents and responsibilities. There is no individual ‘share’ of the rent written into the agreement. The guarantor therefore has the same responsibility.   
+			</p>
+			 <p>
+				<b>Why do I have to pay a deposit?</b> 
+				<br>
+			The landlord trusts the tenant to keep the property in a good condition and in good order. The deposit is held to ensure that any damages (over and above fair wear and tear) can be corrected at the end of the tenancy.
+			</p>
+			 <p>
+			 	<b>What will happen to my deposit?</b> 
+				<br>
+			Landlords and letting agents are required to register your deposit with an approved Tenancy Deposit Scheme. Your Move landlords register their deposits with a scheme such as My Deposits. The deposit is then either held by the landlord, the agent or the deposit scheme itself. You should receive details of the scheme, explaining where the deposit is held.
+			</p>
+			<p>
+				<b>What does a Tenancy Deposit Scheme do?</b> 
+				<br>
+			A Tenancy Deposit Scheme like My Deposits will protect the money for you and can offer assistance should there be a dispute about the deposit at the end of the tenancy.
+			</p>
+			<p>
+				<b>What is a tenancy agreement?</b> 
+				<br>
+			A tenancy agreement is a contract signed by both the tenant and the landlord. It outlines all the rules to which both parties must comply.
+			</p>
+			<p>
+			   <b>What is a routine visit?</b>	
+				<br>
+			Your landlord or letting agent will regularly schedule visits to the property. They want to make sure that the property is being looked after and maintained in a good condition, and they’ll be looking for any maintenance issues.
+			</p>
+			<p>
+				<b>Who is responsible for repairs?</b> 
+				<br>
+			The landlord is responsible for maintaining the property in a good state of repair. They will either take care of this directly, or do so via a letting agent – make sure you know who to go to when there’s a fault at the beginning of the tenancy. Check your 'Welcome letter' to find out what service level your landlord has. If it is Tenant Find or Rent Collect, then you will need to talk to your landlord directly. It it is Fully Managed then the agent will help. If you do damage to the property you are expected to cover the cost of putting this right.
+			</p>
+			<p>
+				<b>Can I decorate or make changes to the property?</b> 
+				<br>
+			In most cases, a tenant can only decorate or make changes to the property with the express permission of the landlord. We recommend receiving this permission in writing.
+			</p>
+			<p>
+				<b>What if I accidentally cause damage to the property?</b> 
+				<br>
+			Don’t worry – accidents happen. Tell whoever is responsible for the property maintenance (either the landlord or letting agent) as soon as possible. You will be expected to cover the cost of putting it right. Don’t try to ignore or hide damage because it could get worse, and it will only come out of your deposit at the end of the tenancy.
+			</p>
+			<p>
+				<b> What if the landlord isn't keeping to their side of the agreement?</b>
+				<br>
+			If a tenant believes the landlord is not keeping to their side of the agreement – for instance, not maintaining the property in a fit state of repair – then the first thing the tenant should do is speak to their letting agent. The letting agent has a duty of care to the tenant, and may be able to help to resolve issues depending on the service type the landlord has with the agent. Look at your 'Welcome letter' to find out the service level of your landlord. Alternatively, a tenant can find independent advice from The Citizens Advice Bureau.
+			</p>
+			<p>
+				<b> When can my landlord enter the property?</b>
+				<br>
+			A landlord has to give the tenant notice before entering the property, unless it’s an emergency.
+			</p>
+			<p>
+			   <b>What if I want to end the tenancy?</b>	
+				<br>
+			If you are tied into a fixed term contract, you will be liable for the rent until the fixed term is finished. If you are no longer in a fixed term contract (ie. a rolling contract) your tenancy agreement will define the notice you need to give.
+			</p>
+			<p>
+				<b>What if I can't pay my rent?</b> 
+				<br>
+			It is always your responsibility to pay the rent, but circumstances change. What happens if you become unemployed or are unable to work due to sickness? The most important thing is not to let arrears pile up until they’re unmanageable. Speak to your landlord or letting agent and see if you can reschedule your payments. And don’t forget, you can get insured against sickness and unemployment to keep yourself protected. Click here to find out more.
+			</p>
+
+		</div>
+	</div>
+
+
+    	<!-- Footer -->
+ 		<?php
+    	
+		include("footer.php");
+	
+		// include("../includethings/footertag.php");
+		?>
+		<!-- Footer -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
