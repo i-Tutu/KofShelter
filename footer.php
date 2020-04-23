06cf6a3f00f49e326ad1be19a71fc1e70e655c70
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 02:59:00 +0000

    Footer file

---
+++ footer.php
@@ -0,0 +1,90 @@
+
+	
+
+			   
+			<!-- Footer -->
+<footer class="page-footer mt-4">  
+
+  <!-- Footer Links -->
+  <div class="container text-center"> 
+    <!-- Grid row -->
+    <div class="row"> 
+    	<!-- Logo -->
+    	<div class="col-md-3 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1" data-aos="fade-up" data-aos-delay="100">      
+     		<a class="navbar-brand logo mt-5" href="index.php">KofShelter</a>
+     		<small><p class="coloreasy">Easily rent in Koforidua</p></small>    
+     	</div>
+      <!-- Grid column -->
+      
+       <div class="col-md-3 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+        <!-- Links -->
+        <small>
+        <h5 class="color" data-aos="fade-in" data-aos-delay="100">About</h5> <!-- font-weight-bold -->
+     	<ul class="list-unstyled" data-aos="fade-right" data-aos-delay="100">
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="termandcon.php">Terms and Condition</a></li>
+          <li><a href="privacypolicy.php">Privacy Policy</a></li>
+        </ul>
+        </small>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="clearfix w-100 d-md-none"> 
+
+      <div class="col-md-3 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">     
+    	<h5 class="color" data-aos="fade-in" data-aos-delay="100">Lets Connect</h5>      
+    	<ul class="list-unstyled" data-aos="fade-down" data-aos-delay="100">        
+    		<li><a href="#"><i class="fa fa-facebook"></i> </a></li>        
+    		<li><a href="#"><i class="fa fa-twitter"></i> </a></li>        
+    		<li><a href="#"><i class="fa fa-instagram"></i> </a></li>      
+    	</ul>   
+    </div>
+
+      <!-- Grid column -->
+      <hr class="clearfix w-100 d-md-none"> 
+
+      <!-- Grid column -->
+       <div class="col-md-3 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+        <!-- Contact details -->
+        <small>
+       <h5 class="color" data-aos="fade-in" data-aos-delay="100">Support</h5>
+        <ul class="list-unstyled" data-aos=>
+          <li><a href="mailto:info@suaponbia.com">Support@kofshelter.com</a></li>
+          <li><a href="contact.php">Contact Us</a>
+          </li>
+          <li><a href="safety_tips.php">Safety tips</a></li>
+          <li><a href="faq.php">FAQ</a> </li>
+        </ul>
+      </small>
+      </div>
+      <!-- Grid column -->
+
+     <hr class="clearfix w-100 d-md-none">
+
+
+    </div> 
+    <!-- Grid row -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center pb-3"><small>KofShelter - Powered by Softtap with <i class="fa fa-heart"></i></small>  
+  </div>
+  <!-- Copyright -->
+
+  </div> 
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
+
+
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>  -->
+
+  <script src="jquery/jquery.min.js"></script>
+	<script src="bootstrap/js/aos.js"></script>
+ 
+	<script>
+		//AOS instance
+		AOS.init();
+
+	</script>
