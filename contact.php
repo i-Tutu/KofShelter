30355e30190a962541c23ea5b800a8b43f6b546a
i-Tutu <ignatiustutu@gmail.com>
2020-04-23 03:05:41 +0000

    Contact file

---
+++ contact.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">    
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>KofShelter - Contact</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+<body>
+
+	   <?php
+			include("head.php");
+		?>
+
+
+	<div class="card bg-light mt-5 mr-5 ml-5">
+	   <div class="card-header">Contact Us</div>
+	    <div class="card-body">
+	    	<p>
+	    		<b> Please feel free to contact us </b>
+	    	</p>
+			<p>
+	    	-	Koforidua Technical University
+	    		<br>
+	    	 	Computer Network Management
+	    		<br> 
+	    	 	Level 300
+	    		<br>
+	    	 	Softtap Group 6
+	    	
+	    	</p>
+
+	    	<p>
+	    	-	Email: info@ktu.edu.gh
+	    	</p>
+
+	    	<p>
+	    	-	Phone: 0540395650
+	    	</p>
+
+		</div>
+	</div>
+
+		<!-- Footer -->
+ 		<?php
+    	
+		include("footer.php");
+	
+		// include("../includethings/footertag.php");
+		?>
+		<!-- Footer -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/\jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/\bootstrap.min.js" crossorigin="anonymous"></script>      
+</body>
+
+</html>
